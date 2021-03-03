bc9d88b2cf4be24cee7d383b539fac0b4f92d063
caiomachhado <caio@bocejinho.com.br>
2021-03-03 20:50:54 -0300

    Criado.

---
+++ app/src/main/java/com/example/gasool_teste/ConsumoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gasool_teste
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConsumoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_consumo)
+    }
+}
